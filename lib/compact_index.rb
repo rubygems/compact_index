8b368ce7ee078f655490f40516818dda1eaa7d37
Felipe Tanus <fotanus@gmail.com>
2015-06-26 18:11:56 -0300

    Move response bodies to this gem

--- lib/compact_index.rb
+++ lib/compact_index.rb
@@ -3,5 +3,32 @@ require "compact_index/versions_file"
 require "compact_index/gem_info"
 
 module CompactIndex
-  # Your code goes here...
+  def self.names(conn)
+    "---\n" + GemInfo.new(conn).names.join("\n") + "\n"
+  end
+
+  def self.versions(conn)
+    VersionsFile.new(conn).with_new_gems
+  end
+
+  def self.info(conn, name)
+    output = "---\n"
+    name = [name] if name.kind_of? String
+    GemInfo.new(conn).deps_for(name).each do |row|
+      output << version_line(row) << "\n"
+    end
+    output
+  end
+
+  private
+    def self.version_line(row)
+      deps = row[:dependencies].map do |d|
+        [d.first, d.last.gsub(/, /, "&")].join(":")
+      end
+
+      line = row[:number].to_s
+      line << "-#{row[:platform]}" unless row[:platform] == "ruby"
+      line << " " << deps.join(",") if deps.any?
+      line
+    end
 end
--- spec/compact_index_spec.rb
+++ spec/compact_index_spec.rb
@@ -1,7 +1,54 @@
 require 'spec_helper'
+require 'support/gem_builder'
 
 describe CompactIndex do
+  let(:builder) { GemBuilder.new($db) }
+
   it 'has a version number' do
     expect(CompactIndex::VERSION).not_to be nil
+  end
+
+  describe '.names' do
+    before do
+      %w(a b c d).each {|gem_name| builder.create_rubygem(gem_name) }
+    end
+    it "returns the gems list" do
+      expected_output = "---\na\nb\nc\nd\n"
+      expect(CompactIndex.names($db)).to eq(expected_output)
+    end
+  end
+
+  describe '.versions' do
+    let(:data) { "a 1.0.0,1.0.1\nb 1.0.0\nc 1.0.0-java\na 2.0.0\na 2.0.1" }
+    before do
+      allow_any_instance_of(CompactIndex::VersionsFile).to receive(:with_new_gems).and_return(data)
+    end
+    it "returns versions.list" do
+      expect(CompactIndex.versions($db)).to eq(data)
+    end
+  end
+
+  describe '.info' do
+    before do
+      rack_id = builder.create_rubygem("rack")
+      builder.create_version(rack_id, "rack")
+      rack_101 = builder.create_version(rack_id, 'rack', '1.0.1')
+      [['foo', '= 1.0.0'], ['bar', '>= 2.1, < 3.0']].each do |dep, requirements|
+        dep_id = builder.create_rubygem(dep)
+        builder.create_dependency(dep_id, rack_101, requirements)
+      end
+    end
+
+    let(:expected_deps) do
+      <<-DEPS.gsub(/^ */, '')
+        ---
+        1.0.0
+        1.0.1 bar:>= 2.1&< 3.0,foo:= 1.0.0
+      DEPS
+    end
+
+    it "should return the gem list" do
+      expect(CompactIndex.info($db, 'rack')).to eq(expected_deps)
+    end
   end
 end
